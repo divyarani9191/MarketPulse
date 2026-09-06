ecbc1fdb14999512a2fe40f31f117108c95279f3
Divya <divyaraninaw@gmail.com>
2026-09-06 22:53:08 +0530

    Improve fintech dashboard UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -437,7 +437,7 @@ function App() {
       <nav className="navbar">
 
         <div className="logo">
-          <span className="logo-icon">◉</span>
+          <span className="logo-icon">↗</span>
           <span>MarketPulse</span>
         </div>
 
